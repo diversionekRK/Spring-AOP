8b2142d71215f742f6072844ef520236b44eb2de
RK <rafal.klewek@pollub.edu.pl>
2018-04-17 18:42:39 +0200

    Migrate simple example to annotation based example

--- src/com/div/spring/aop/Camera.java
+++ src/com/div/spring/aop/Camera.java
@@ -1,8 +1,12 @@
 package com.div.spring.aop;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Div on 2018-04-17.
  */
+
+@Component
 public class Camera {
     public void snap() {
         System.out.println("SNAP!");
--- src/com/div/spring/aop/Logger.java
+++ src/com/div/spring/aop/Logger.java
@@ -1,9 +1,23 @@
 package com.div.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Div on 2018-04-17.
  */
+
+@Aspect
+@Component
 public class Logger {
+
+    @Pointcut("execution(void com.div.spring.aop.Camera.snap())")
+    private void cameraSnap() {}
+
+    //@Before("execution(void com.div.spring.aop.Camera.snap())")
+    @Before("cameraSnap()")
     public void aboutToTakePhoto() {
         System.out.println("About to take a photo.");
     }
